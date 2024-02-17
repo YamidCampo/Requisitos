658e6624bc907abe72d0c378e248bf3e713ef4bc
JohanOrtega28 <125934568+JohanOrtega28@users.noreply.github.com>
2024-02-17 11:45:22 -0500

    Funcion_enter

--- script.js
+++ script.js
@@ -1,81 +1,84 @@
-let interpretacionCajon = document.getElementById("interpretacionDiv")
-interpretacionCajon.classList.add("noMostrar")
+let interpretacionCajon = document.getElementById("interpretacionDiv");
+interpretacionCajon.classList.add("noMostrar");
 
-
-function calcularImc(){
-    let altura = parseFloat(document.getElementById("alturaUsuario").value)
-    let peso = parseFloat(document.getElementById("pesoUsuario").value)
-    console.log(peso)
-
-    const elementErrorAltura = document.getElementById('errorAlturaUsuario')
-    const elementErrorPeso = document.getElementById('errorPesoUsuario')
-
-    elementErrorAltura.textContent = ''
-    elementErrorPeso.textContent = ''
-
-    if (isNaN(altura)){
-      elementErrorAltura.textContent = 'Debes ingresar la altura'
-      return
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+        interpretacion();
     }
-    if(isNaN(peso)) {
-      elementErrorPeso.textContent = 'Debes ingresar el peso'
-      return
+});
+
+function calcularImc() {
+    let altura = parseFloat(document.getElementById("alturaUsuario").value);
+    let peso = parseFloat(document.getElementById("pesoUsuario").value);
+    console.log(peso);
+
+    const elementErrorAltura = document.getElementById('errorAlturaUsuario');
+    const elementErrorPeso = document.getElementById('errorPesoUsuario');
+
+    elementErrorAltura.textContent = '';
+    elementErrorPeso.textContent = '';
+
+    if (isNaN(altura)) {
+        elementErrorAltura.textContent = 'Debes ingresar la altura';
+        return;
     }
-    if (altura < 0.8 || altura > 3){
-      elementErrorAltura.textContent = "La altura debe estar entre 0.8 y 3 metros"
-        return
-    }   
-    if (peso < 15 || peso > 700){
-      elementErrorPeso.textContent = "El peso debe estar entre 15 y 700 kg"
-      return
+    if (isNaN(peso)) {
+        elementErrorPeso.textContent = 'Debes ingresar el peso';
+        return;
+    }
+    if (altura < 0.8 || altura > 3) {
+        elementErrorAltura.textContent = "La altura debe estar entre 0.8 y 3 metros";
+        return;
+    }
+    if (peso < 15 || peso > 700) {
+        elementErrorPeso.textContent = "El peso debe estar entre 15 y 700 kg";
+        return;
     }
 
-    // Aqui el calculo
-    let imc = peso / (altura**2);
-    imc= imc.toFixed(2);
-    return imc
-
+    // Aquí el cálculo
+    let imc = peso / (altura ** 2);
+    imc = imc.toFixed(2);
+    return imc;
 }
 
-function intepretacion(){
-    let imc = calcularImc()
+function interpretacion() {
+    let imc = calcularImc();
     let resultado;
 
-    if(!imc) {
-      cerrarInterpretacion()
-      return
+    if (!imc) {
+        cerrarInterpretacion();
+        return;
     }
 
     switch (true) {
         case (imc < 18.5):
-           resultado = "se encuentra dentro del rango de peso bajo"; 
-           break
+            resultado = "se encuentra dentro del rango de peso bajo";
+            break;
         case (imc >= 18.5 && imc < 25):
-          resultado = "se encuentra dentro del rango de peso normal o saludable";
-          break;
+            resultado = "se encuentra dentro del rango de peso normal o saludable";
+            break;
         case (imc >= 25.0 && imc < 30):
-          resultado = "se encuentra dentro del rango de sobrepeso. ";
-          break;
+            resultado = "se encuentra dentro del rango de sobrepeso. ";
+            break;
         case (imc >= 30 && imc < 35):
-          resultado = "se encuentra dentro del rango de obesidad I";
-          break;
+            resultado = "se encuentra dentro del rango de obesidad I";
+            break;
         case (imc >= 35 && imc < 40):
-          resultado = "se encuentra dentro del rango de obesidad II. ";
-          break;
+            resultado = "se encuentra dentro del rango de obesidad II. ";
+            break;
         default:
-          resultado = "se encuentra dentro del rango de obesidad III.";
-          break;
+            resultado = "se encuentra dentro del rango de obesidad III.";
+            break;
     }
 
-    let cajonInterpretacion = document.getElementById("cajaInterpretacion")
-    let cajonMostrarImc = document.getElementById("mostrarImc")
-    cajonInterpretacion.textContent = resultado
-    cajonMostrarImc.textContent = "Su IMC es de: " + imc
+    let cajonInterpretacion = document.getElementById("cajaInterpretacion");
+    let cajonMostrarImc = document.getElementById("mostrarImc");
+    cajonInterpretacion.textContent = resultado;
+    cajonMostrarImc.textContent = "Su IMC es de: " + imc;
 
-    interpretacionCajon.classList.remove("noMostrar")
+    interpretacionCajon.classList.remove("noMostrar");
 }
 
-
-function cerrarInterpretacion(){
-  interpretacionCajon.classList.add("noMostrar")
+function cerrarInterpretacion() {
+    interpretacionCajon.classList.add("noMostrar");
 }
